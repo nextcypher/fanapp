cd858cd2beb494a5b565762b6a7d88a5730257dc
VadymCodes <skoryivadym@gmail.com>
2023-03-02 21:46:57 +0200

    fix: my collection loading

--- src/components/NFTCard.js
+++ src/components/NFTCard.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 const NFTCard = ({ nft, flag = true }) => {
+  function convert(num) {
+    switch (num.toString().length) {
+      case 1: return `000${num}`
+      case 2: return `00${num}`
+      case 3: return `0${num}`
+      case 4: return num
+      default: return "0000"
+    }
+  }
   return (
     <div className="max-w-lg rounded overflow-hidden shadow-lg">
-      <img src={nft.image} alt="" className="w-full" />
+      <img src={`https://nxc-public.s3.amazonaws.com/tn/${convert(nft.edition)}.jpg`} alt="" className="w-full" />
       {flag && (
         <div className="px-4 py-4">
           <div className="font-bold text-teal-600 text-xl mb-2">{nft.name}</div>
